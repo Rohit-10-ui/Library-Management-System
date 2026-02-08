abcf036352644821764891b753897989cd2ffcaa
Rohit-10-ui <rohite477@gmail.com>
2026-02-08 16:49:17 +0530

    Landing Page ---V1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Landing from "./Landing";
 import Login from "./Login";
 import Signup from "./Signup";
 
@@ -7,7 +8,8 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
     </BrowserRouter>
