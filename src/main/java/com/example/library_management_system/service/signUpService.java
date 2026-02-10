c1ef12a8b799680addb9a396ef9c478f90c27838
PrashantPulkit <prashantpulkit5@gmail.com>
2026-02-10 13:45:28 +0530

    sign up service interface

--- src/main/java/com/example/library_management_system/service/signUpService.java
+++
@@ -1,8 +0,0 @@
-package com.example.library_management_system.service;
-
-import org.springframework.web.multipart.MultipartFile;
-
-
-public interface SignupService<T> {
-    void signup(T req, MultipartFile idProof);
-}
